37e7791b1b24f2df32e9391ebf85647e95eb7e4c
shalomjaison <sjacobjaison@gmail.com>
2025-09-23 08:26:20 +0400

    Added query engine

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ['@prisma/client', 'prisma'],
   outputFileTracingIncludes: {
     '/api/**/*': ['./src/generated/prisma/**/*'],
+    '/dashboard/**': ['./src/generated/prisma/**/*'],
+    '/**': ['./src/generated/prisma/query-engine-rhel-openssl-3.0.x'],
+  },
+  experimental: {
+    outputFileTracingRoot: '.',
   },
 };
 
