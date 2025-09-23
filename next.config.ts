28806186320cc7aae265beb003b0beadd6a3edf4
shalomjaison <sjacobjaison@gmail.com>
2025-09-23 07:43:01 +0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        '@prisma/client': '@prisma/client',
+      });
+    }
+    return config;
+  },
+  // Ensure Prisma binaries are included in the deployment
+  outputFileTracing: true,
 };
 
 export default nextConfig;
